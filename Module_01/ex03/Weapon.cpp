4841628b5ec8895d1461c95443c6206c1fe97cb6
Florent Belotti <florent.l.d.belotti@gmail.com>
2024-09-18 14:31:57 +0200

    a little change

--- Module_01/ex03/Weapon.cpp
+++ Module_01/ex03/Weapon.cpp
@@ -6,13 +6,13 @@
 /*   By: fbelotti <marvin@42perpignan.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/17 17:46:44 by fbelotti          #+#    #+#             */
-/*   Updated: 2024/09/18 13:53:13 by fbelotti         ###   ########.fr       */
+/*   Updated: 2024/09/18 14:30:41 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.h"
 
-Weapon::Weapon(std::string type) : type(type) {}
+Weapon::Weapon(std::string type_of_weapon) : type(type_of_weapon) {}
 
 Weapon::~Weapon() {
 	std::cout << this->type << " is destroyed" << std::endl;
--- Module_01/ex03/Weapon.hpp
+++ Module_01/ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: fbelotti <marvin@42perpignan.fr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 17:56:26 by fbelotti          #+#    #+#             */
-/*   Updated: 2024/09/18 13:52:42 by fbelotti         ###   ########.fr       */
+/*   Updated: 2024/09/18 14:30:26 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ class Weapon {
 	std::string type;
 
 	public:
-	Weapon(std::string type);
+	Weapon(std::string type_of_weapon);
 	~Weapon();
 
 	const std::string& getType() const;
