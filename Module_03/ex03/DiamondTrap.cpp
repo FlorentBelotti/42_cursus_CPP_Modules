a8f73b2518165de7b892371051be2751ea2d7e83
Florent Belotti <florent.l.d.belotti@gmail.com>
2024-09-30 13:46:32 +0200

    color

--- Module_03/ex03/DiamondTrap.cpp
+++ Module_03/ex03/DiamondTrap.cpp
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   DiamondTrap.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fbelotti <marvin@42perpignan.fr>           +#+  +:+       +#+        */
+/*   By: fbelotti <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/26 17:02:11 by fbelotti          #+#    #+#             */
-/*   Updated: 2024/09/26 17:14:02 by fbelotti         ###   ########.fr       */
+/*   Updated: 2024/09/30 13:31:49 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "DiamondTrap.hpp"
 
 DiamondTrap::DiamondTrap(std::string name)
-	: ClapTrap(name + "_clap_name"), ScavTrap(name), FragTrap(name), _name(name) {
+	: ClapTrap(name), ScavTrap(name), FragTrap(name), _name(name) {
 	setHitPoints(FragTrap::getHitPoints());
 	setEnergyPoints(ScavTrap::getEnergyPoints());
 	setAttackDamage(FragTrap::getAttackDamage());
-	std::cout << "DiamondTrap " << getName() << " constructed." << std::endl;
+	std::cout << RED << "DiamondTrap " <<  getName() << RESET_COLOR << " created." << std::endl;
 }
 
 DiamondTrap::~DiamondTrap() {
-	std::cout << "DiamondTrap " << getName() << " destroyed." << std::endl;
+	std::cout << RED << "DiamondTrap " <<  getName() << RESET_COLOR <<" destroyed." << std::endl;
 }
 
 void DiamondTrap::whoAmI() {
@@ -30,32 +30,32 @@ void DiamondTrap::whoAmI() {
 
 void DiamondTrap::attack(const std::string& target) {
 	if (getEnergyPoints() > 0) {
-		std::cout << ORANGE << "DiamondTrap " << getName() << RESET_COLOR << " attacks " << target << ", causing " << getAttackDamage() << " points of damage!" << std::endl;
+		std::cout << RED << "DiamondTrap " << getName() << RESET_COLOR << " attacks " << target << ", causing " << getAttackDamage() << " points of damage!" << std::endl;
 		setEnergyPoints(getEnergyPoints() - 1);
-		std::cout << ORANGE << "DiamondTrap " << getName() << YELLOW << " " << getEnergyPoints() << " energy points remaining." << RESET_COLOR << std::endl;
+		std::cout << RED << "DiamondTrap " << getName() << YELLOW << " " << getEnergyPoints() << " energy points remaining." << RESET_COLOR << std::endl;
 	} else {
-		std::cout << ORANGE << "DiamondTrap " << getName() << RESET_COLOR << " has 0 energy points and can't attack." << std::endl;
+		std::cout << RED << "DiamondTrap " << getName() << RESET_COLOR << " has 0 energy points and can't attack." << std::endl;
 	}
 }
 
 void DiamondTrap::beRepaired(unsigned int amount) {
 	if (getEnergyPoints() > 0) {
-		std::cout << ORANGE << "DiamondTrap " << getName() << RESET_COLOR << " repairs himself of " << amount << " hit points " << std::endl;
+		std::cout << RED << "DiamondTrap " << getName() << RESET_COLOR << " repairs himself of " << amount << " hit points " << std::endl;
 		setEnergyPoints(getEnergyPoints() - 1);
-		std::cout << ORANGE << "DiamondTrap " << getName() << YELLOW << " " << getEnergyPoints() << " energy points remaining." << RESET_COLOR << std::endl;
+		std::cout << RED << "DiamondTrap " << getName() << YELLOW << " " << getEnergyPoints() << " energy points remaining." << RESET_COLOR << std::endl;
 		setHitPoints(getHitPoints() + amount);
-		std::cout << ORANGE << "DiamondTrap " << getName() << YELLOW << " " << getHitPoints() << " hit points remaining." << RESET_COLOR << std::endl;
+		std::cout << RED << "DiamondTrap " << getName() << YELLOW << " " << getHitPoints() << " hit points remaining." << RESET_COLOR << std::endl;
 	}
 	else {
-		std::cout << ORANGE << "DiamondTrap " << getName() << RESET_COLOR << " has 0 energy point and can't attack " << std::endl;
+		std::cout << RED << "DiamondTrap " << getName() << RESET_COLOR << " has 0 energy point and can't attack " << std::endl;
 	}
 }
 
 void DiamondTrap::takeDamage(unsigned int amount) {
 	if (getHitPoints() > 0) {
-		std::cout << ORANGE << "DiamondTrap " << getName() << RESET_COLOR << " take "<< amount << " points of damage!" << std::endl;
+		std::cout << RED << "DiamondTrap " << getName() << RESET_COLOR << " take "<< amount << " points of damage!" << std::endl;
 		setHitPoints(getHitPoints() - amount);
-		std::cout << ORANGE << "DiamondTrap " << getName() << YELLOW << " " << getHitPoints() << " hit points remaining." << RESET_COLOR << std::endl;
+		std::cout << RED << "DiamondTrap " << getName() << YELLOW << " " << getHitPoints() << " hit points remaining." << RESET_COLOR << std::endl;
 	}
 	else {
 		DiamondTrap::~DiamondTrap();
