e6c177452a1974bc414c3c24aa2f01750669ec0d
Florent Belotti <florent.l.d.belotti@gmail.com>
2024-10-23 12:13:21 +0200

    ex02 m7

--- Module_07/ex02/Array.hpp
+++ Module_07/ex02/Array.hpp
@@ -6,9 +6,12 @@
 /*   By: fbelotti <fbelotti@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 17:59:06 by fbelotti          #+#    #+#             */
-/*   Updated: 2024/10/22 18:25:22 by fbelotti         ###   ########.fr       */
+/*   Updated: 2024/10/23 12:08:42 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <iostream>
+#include <stdexcept>
 
 #pragma once
 template <typename T>
@@ -35,7 +38,75 @@ class Array {
 
     // Operator
     Array &operator=(const Array &other);
-
+    T &operator[](unsigned int index);
+    const T &operator[](unsigned int index) const;
+    
     // Getters
     unsigned int size() const;
+    T* get_array() const;
 };
+
+// Constructor : Without parameter, allocate an array
+template <typename T>
+Array<T>::Array() : _array(NULL), _size(0) {}
+
+// Constructor : With parameter, allocate an array of size n
+template <typename T>
+Array<T>::Array(unsigned int n) : _array(new T[n]), _size(n) {}
+
+// Copy constructor
+template <typename T>
+Array<T>::Array(const Array &other) : _array(new T[other._size]), _size(other._size) {
+    for (unsigned int i = 0; i < _size; i++)
+        _array[i] = other._array[i];
+}
+
+// Destructor
+template <typename T>
+Array<T>::~Array() {
+    if (_array)
+        delete[] _array;
+}
+
+// Assignment operator
+template <typename T>
+Array<T> &Array<T>::operator=(const Array &other) {
+    if (this != &other) {
+        if (_array)
+            delete[] _array;
+        _array = new T[other._size];
+        _size = other._size;
+        for (unsigned int i = 0; i < _size; i++)
+            _array[i] = other._array[i];
+    }
+    return *this;
+}
+
+// Non-const subscript operator
+template <typename T>
+T &Array<T>::operator[](unsigned int index) {
+    if (index >= _size) {
+        throw std::out_of_range("Index out of bounds");
+    }
+    return _array[index];
+}
+
+// Const subscript operator
+template <typename T>
+const T &Array<T>::operator[](unsigned int index) const {
+    if (index >= _size) {
+        throw std::out_of_range("Index out of bounds");
+    }
+    return _array[index];
+}
+
+// Getters
+template <typename T>
+unsigned int Array<T>::size() const {
+    return _size;
+}
+
+template <typename T>
+T* Array<T>::get_array() const {
+    return _array;
+}
--- Module_07/ex02/main.cpp
+++ Module_07/ex02/main.cpp
@@ -6,12 +6,50 @@
 /*   By: fbelotti <fbelotti@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 18:28:55 by fbelotti          #+#    #+#             */
-/*   Updated: 2024/10/22 18:30:39 by fbelotti         ###   ########.fr       */
+/*   Updated: 2024/10/23 12:12:49 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
+#include "Color.hpp"
+#include <iostream>
 
 int main(void) {
-    
+    try {
+        Array<int> defaultArray;
+        std::cout << YELLOW << "Size of defaultArray" << " : " << RESET_COLOR << defaultArray.size() << std::endl;
+
+        Array<int> paramArray(5);
+        std::cout << YELLOW << "Size of paramArray" << " : " << RESET_COLOR << paramArray.size() << std::endl;
+
+        for (unsigned int i = 0; i < paramArray.size(); ++i) {
+            paramArray[i] = i * 10;
+        }
+
+        std::cout << BLUE << "Elements of paramArray: " << RESET_COLOR;
+        for (unsigned int i = 0; i < paramArray.size(); ++i) {
+            std::cout << paramArray[i] << ", ";
+        }
+        std::cout << std::endl;
+
+        Array<int> copyArray(paramArray);
+        std::cout << YELLOW << "Size of copyArray" << " : " << RESET_COLOR << copyArray.size() << std::endl;
+
+        std::cout << MAGENTA << "Elements of copyArray: " << RESET_COLOR;
+        for (unsigned int i = 0; i < copyArray.size(); ++i) {
+            std::cout << copyArray[i] << ", ";
+        }
+        std::cout << std::endl;
+
+        paramArray[0] = 100;
+        std::cout << BLUE << "Modified paramArray[0]: " << RESET_COLOR << paramArray[0] << std::endl;
+        std::cout << MAGENTA << "Unmodified copyArray[0]: " << RESET_COLOR << copyArray[0] << std::endl;
+
+        std::cout << RED << "Attempting out-of-bounds access: " << RESET_COLOR;
+        std::cout << paramArray[10] << std::endl; // This should throw an exception
+
+    } catch (const std::exception& e) {
+        std::cerr << RESET_COLOR << "Exception: " << e.what() << RESET_COLOR << std::endl;
+    }
+    return 0;
 }
