7ea89e03790e8719176e974f0d0584dcb9ac3109
Florent Belotti <florent.l.d.belotti@gmail.com>
2024-10-25 01:48:48 +0200

    main ready to execute

--- Module_08/ex02/main.cpp
+++ Module_08/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: fbelotti <fbelotti@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 00:56:16 by fbelotti          #+#    #+#             */
-/*   Updated: 2024/10/25 01:21:55 by fbelotti         ###   ########.fr       */
+/*   Updated: 2024/10/25 01:44:59 by fbelotti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,37 +18,44 @@ int main()
     MutantStack<int> mstack;
 
     // Pousser des éléments dans la pile
+    std::cout << YELLOW << "\n----- Pushing two elements -----\n" << RESET_COLOR << std::endl;
     mstack.push(5);
     mstack.push(17);
+    std::cout << YELLOW << "Stack size: " << RESET_COLOR << mstack.size() << std::endl;
 
     // Afficher l'élément au sommet de la pile
     std::cout << GREEN << "Top element: " << RESET_COLOR << mstack.top() << std::endl;
 
     // Retirer l'élément au sommet de la pile
+    std::cout << RED << "\n----- Deleting one element -----\n" << RESET_COLOR << std::endl;
     mstack.pop();
 
     // Afficher la taille de la pile
     std::cout << YELLOW << "Stack size: " << RESET_COLOR << mstack.size() << std::endl;
+    std::cout << GREEN << "Top element: " << RESET_COLOR << mstack.top() << std::endl;
 
     // Ajouter d'autres éléments
+    std::cout << YELLOW << "\n----- Pushing four elements -----" << RESET_COLOR << std::endl;
     mstack.push(3);
     mstack.push(5);
-    mstack.push(737);
-    mstack.push(0);
+    mstack.push(42);
+    mstack.push(24601);
 
     // Afficher la taille de la pile
-    std::cout << YELLOW << "Stack size: " << RESET_COLOR << mstack.size() << std::endl;
+    std::cout << YELLOW << "\nStack size: " << RESET_COLOR << mstack.size() << std::endl;
+    std::cout << GREEN << "Top element: " << RESET_COLOR << mstack.top() << std::endl;
 
     // Utiliser des itérateurs pour parcourir la pile
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
 
-    std::cout << YELLOW << "\n----- Stack elements -----" << RESET_COLOR << std::endl;
+    std::cout << BLUE << "\n----- Stack elements -----" << RESET_COLOR << std::endl;
     while (it != ite) {
-        std::cout << *it << std::endl;
+        if (it == ite - 1)
+            std::cout << GREEN << *it << RESET_COLOR << std::endl;
+        else
+            std::cout << *it << std::endl;
         ++it;
     }
     return 0;
-
-    std::stack<int> s(mstack);
 }
